6a1e9059b8c6a5fd8129d6b1802174d1b9ef5318
Antonius Torode <24546058+torodean@users.noreply.github.com>
2023-08-30 09:26:37 -0500

    Update update_readme.js

--- scripts/update_readme.js
+++ scripts/update_readme.js
@@ -2,40 +2,28 @@
 const fs = require('fs');
 const path = require('path');
 
-// Root directory to start processing
 const ROOT_DIR = './workouts/';
 const README_FILENAME = 'README.md';
-const NB_IMAGES_PER_LINE = 4;
 
-// Recursive function to process directories
-function processDirectory(directory) {
-  let nbImages = 0;
-  let mdContent = '<table><tr>';
-
+function getImagesRecursive(directory, images = []) {
   fs.readdirSync(directory).forEach((item) => {
     const itemPath = path.join(directory, item);
     const stats = fs.statSync(itemPath);
 
-    if (stats.isFile() && item !== README_FILENAME) {
-      if (!(nbImages % NB_IMAGES_PER_LINE)) {
-        if (nbImages > 0) {
-          mdContent += '</tr>';
-        }
-        mdContent += '<tr>';
-      }
-      nbImages++;
-      mdContent += `
-<td valign="bottom">
-<img src="./${item}" width="200"><br>
-${item}
-</td>`;
+    if (stats.isFile() && (item.endsWith('.jpg') || item.endsWith('.png'))) {
+      images.push(path.relative(ROOT_DIR, itemPath));
     } else if (stats.isDirectory()) {
-      processDirectory(itemPath); // Recursively process subdirectories
+      getImagesRecursive(itemPath, images);
     }
   });
 
-  mdContent += '</tr></table>';
-  fs.writeFileSync(path.join(directory, README_FILENAME), mdContent);
+  return images;
 }
 
-processDirectory(ROOT_DIR);
+const imageList = getImagesRecursive(ROOT_DIR);
+const mdContent = imageList.map((image) => {
+  return `![${image}](${image})`;
+}).join('\n');
+
+fs.writeFileSync(path.join(ROOT_DIR, README_FILENAME), mdContent);
+
